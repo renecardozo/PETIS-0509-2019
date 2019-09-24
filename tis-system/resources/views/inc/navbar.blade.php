0b27011d90a6b7e59d9765483246815cec7151a8
dsiles98 <dayanasiles98@gmail.com>
2019-09-24 04:06:35 -0400

    Home and navbar

--- tis-system/resources/views/inc/navbar.blade.php
+++ tis-system/resources/views/inc/navbar.blade.php
@@ -7,14 +7,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/">{{config('app.name', 'SISTEMA')}}</a>
+        
+        <a class="navbar-brand" href="/">KirinSoft</a>
     </div>
     <div id="navbar" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/post">Publicaciones</a></li>
+        <li><a href="/">Recientes</a></li>
+        <li><a href="/">Favoritos</a></li>
+        <li><a href="/post">Escribenos</a></li>
         </ul>
     </div>
     </div>
-</nav>g
+</nav>
